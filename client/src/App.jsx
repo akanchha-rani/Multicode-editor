138d8b6a6eb61d56a90399bbc3b8a7d5b3eee14e
akanchha-rani <akanchha134@gmail.com>
2025-03-20 13:11:23 +0530

    updated app.jsx
    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Editor from './pages/Editor';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <RouteHandler />
+      </BrowserRouter>
+    </>
+  )
+};
+
+const RouteHandler = () => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to={"/login"}/>} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/editior/:id" element={isLoggedIn ? <Editor /> : <Navigate to={"/login"}/>} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </>
   )
 }
